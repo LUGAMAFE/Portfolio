62d78c15dddec0e9f62029b4bf40de6ade06003b
Alfonso Tort <alfonso.tort@anahuac.com>
2023-12-19 23:47:08 -0600

    custom styles for input range skill

--- src/components/SkillSection/React-range/index.tsx
+++ src/components/SkillSection/React-range/index.tsx
@@ -1,13 +1,28 @@
 import RangeSlider from 'react-range-slider-input';
 import 'react-range-slider-input/dist/style.css';
+import NeumorphicElement from '../../neumorphic/NeomorphicElement/NeumorphicElement';
+import style from '../Skill/skill.module.scss';
 import './ReactRange.scss';
 export const ReactRange = () => {
   return (
-    <RangeSlider
-      className="single-thumb"
-      defaultValue={[0, 50]}
-      thumbsDisabled={[true, false]}
-      rangeSlideDisabled={true}
-    />
+    <NeumorphicElement
+      neumorphicOptions={{
+        form: 'pressed',
+        size: 102,
+        intensity: 0.76,
+        lightSource: 1,
+        distance: 9,
+        blur: 18,
+      }}
+      className={style.Skill_nullPart}
+    >
+
+      <RangeSlider
+        className="single-thumb"
+        defaultValue={[0, 50]}
+        thumbsDisabled={[true, false]}
+        rangeSlideDisabled={true}
+      />
+    </NeumorphicElement>
   );
 };
