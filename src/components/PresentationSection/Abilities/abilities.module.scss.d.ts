12336bdbda7b6570c581bc97b4207a20d45cfadd
Alfonso Tort <alfonso.tort@anahuac.com>
2024-03-27 01:51:53 -0600

    changes on presentation section

--- src/components/PresentationSection/Abilities/abilities.module.scss.d.ts
+++ src/components/PresentationSection/Abilities/abilities.module.scss.d.ts
@@ -4,7 +4,6 @@ declare const classNames: {
   readonly container: 'container';
   readonly Abilities: 'Abilities';
   readonly Abilities_svgContainer: 'Abilities_svgContainer';
-  readonly '875rem': '875rem';
   readonly Abilities_text: 'Abilities_text';
   readonly Abilities_text___rotated: 'Abilities_text___rotated';
   readonly Abilities_neomorphicSVG: 'Abilities_neomorphicSVG';
